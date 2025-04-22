6c92f6578b6be64da8bed8761bfd825875cbea17
YU <w25225575@gmail.com>
2025-04-22 14:13:37 +0800

    add linebot

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
 
 from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+                   send_from_directory, url_for, abort)
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import PostbackEvent, TextSendMessage, MessageEvent, TextMessage
 
 app = Flask(__name__)
 
 TOKEN = os.environ.get('LINE_TOKEN', None)
 SECRET = os.environ.get('LINE_SECRET', None)
+
+line_bot_api = LineBotApi(TOKEN)
+whhandler = WebhookHandler(SECRET)
 
 @app.route('/')
 def index():
@@ -29,6 +36,25 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+@app.route('/callback', methods=['POST'])
+def callback():
+    # 確認請求來自 LINE
+    signature = request.headers['X-Line-Signature']
+
+    # 獲取請求主體
+    body = request.get_data(as_text=True)
+    app.logger.info(f"Request body: {body}")
+
+    try:
+        whhandler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@whhandler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str(f'{event}')))
 
 @app.route('/hi',methods=['get'])
 def hi():
